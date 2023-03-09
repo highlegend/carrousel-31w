a36c1fcd569c669ab3d104580508c7325b4386f1
Mehdi Said <e1489401@cmaisonneuve.qc.ca>
2023-03-09 16:22:00 -0500

    cours 12 correction du code dans le fichier .js pour les images dans le carrousel

--- js/carrousel.js
+++ js/carrousel.js
@@ -5,34 +5,38 @@
     let elmCarrousel = document.querySelector('.carrousel')
     let elmBouton__x = document.querySelector('.bouton__x')
     let elmGalerie = document.querySelector('.galerie')
-    let elmGalerie__img = document.querySelectorAll('img')
-    let elmCarrousel_figure = document.querySelector ('.carrousel__figure')
+    let elmGalerie__img = elmGalerie.querySelectorAll('img')
+    let elmCarrousel__figure = document.querySelector('.carrousel__figure')
     console.log(elmGalerie__img.length)
-
-
+    
+    
+    
     console.log(bouton__ouvrir.tagName)
     
     bouton__ouvrir.addEventListener('mousedown', function(){
         console.log('boîte modale')
         elmCarrousel.classList.add('carrousel--ouvrir')
-        ajouter_img()
+        ajouter_carrousel()
     })
     elmBouton__x.addEventListener('mousedown', function(){
         console.log('boîte modale')
         elmCarrousel.classList.remove('carrousel--ouvrir')
     })
-
+    
     function ajouter_carrousel()
     {
-     for (const elmImg of elmGalerie__img){
-        ajouter_img(elmImg) //ajoute l'image dans le carrousel
-    }   
+        for (const elmImg of elmGalerie__img){
+            ajouter_img(elmImg) // ajoute l'image dans le carrousel
+        }
     }
-
-    function ajouter_img(elmImg)
-    let elmCarrousel__img = document.createElement('img')
+    
+    function ajouter_img(elmImg){
+        let elmCarrousel__img = document.createElement('img')
         elmCarrousel__img.setAttribute('src', elmImg.getAttribute('src'))
         elmCarrousel__figure.appendChild(elmCarrousel__img)
+    }
+    
+    
     })()
 
 /* (function(){
