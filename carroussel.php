6646c11cb72b960cdb1015163941266f2ef15558
Mehdi Said <e1489401@cmaisonneuve.qc.ca>
2023-03-09 15:30:23 -0500

    cours 12 affichage et animation de la boite modale

--- carroussel.php
+++ carroussel.php
@@ -1,5 +1,51 @@
 <?php
 /**
+* Extension carrousel permet d'afficher dans une boîte modale 
+* les images d'une galerie
+* Package name : Carrousel
+* Version: 1.0.0
+*/
+/*
+Plugin name: Carrousel
+Author: Eddy Martin
+Plugin URI: https:github.com/eddytuto/carrousel
+Description: Permet d'afficher dans une boîte modale les images d'une galerie avec un système de navigation
+*/
+
+
+function carrousel_enqueue(){
+
+$version_css = filemtime(plugin_dir_path(__FILE__ ) . "style.css");
+$version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+
+wp_enqueue_style(   'em_plugin_carrousel_css',
+plugin_dir_url(__FILE__) . "style.css",
+array(),
+$version_css);
+
+wp_enqueue_script(  'em_plugin_carrousel_js',
+plugin_dir_url(__FILE__) ."js/carrousel.js",
+array(),
+$version_js,
+true);
+}
+
+add_action('wp_enqueue_scripts', 'carrousel_enqueue');
+
+function creation_carrousel()
+{
+return '<button class="bouton__ouvrir">Ouvrir</button>
+    <div class="carrousel">
+    <button class="bouton__x">X</button>
+    <figure class="carrousel__figure"></figure>
+    <form class="carrousel__form"></form>
+    </div>';
+}  
+
+add_shortcode('carrousel', 'creation_carrousel');
+
+?>
+/**
  * Extension carrousel permet d'afficher dans une boite mondale
  * les images d'une galerie
  * Package name: Carrousel
@@ -16,34 +62,3 @@ Description: permet d'afficher dans une boite mondale les images d'une galerie a
 Nous ce qu'on doit faire cest perfectionner le systeme de navigation **
 
 */
-function carrousel_enqueue(){
-
-$version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
-$version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
-
-wp_enqueue_style(   'em_plugin_carrousel_css',
-plugin_dir_url(__FILE__) . "style.css",
-array(),
-$version_css);
-
-wp_enqueue_script(  'em_plugin_carrousel_js',
-plugin_dir_url(__FILE__) ."js/carrousel.js",
-array(),
-$version_js,
-true);
-
-}
-
-add_action('wp_enqueue_scripts', 'caroussel_enqueue');
-
-function creation_carrousel(){
-return'<button class="bouton__ouvrir">Ouvrir</button>
-    <div class="carrousel">
-    <button class="bouton__x">X</button>
-    <figure class="carrousel__figure"></figure>
-    <form class="carrousel__form"></form>
-    </div>';  
-}
-
-add_shortcode('carrousel', 'creation_carrousel');
-?>
--- js/carrousel.js
+++ js/carrousel.js
@@ -1,4 +1,43 @@
 (function(){
     // fonction IFEE
+    console.log('début du carrousel')
+    let bouton__ouvrir = document.querySelector('.bouton__ouvrir')
+    let elmCarrousel = document.querySelector('.carrousel')
+    let elmBouton__x = document.querySelector('.bouton__x')
+    let elmGalerie = document.querySelector('.galerie')
+    let elmGalerie__img = document.querySelectorAll('img')
+    console.log(elmGalerie__img.length)
+    for (const elmImg of elmGalerie__img){
+        
+    }
+
+    console.log(bouton__ouvrir.tagName)
+    
+    bouton__ouvrir.addEventListener('mousedown', function(){
+        console.log('boîte modale')
+        elmCarrousel.classList.add('carrousel--ouvrir')
+    })
+    elmBouton__x.addEventListener('mousedown', function(){
+        console.log('boîte modale')
+        elmCarrousel.classList.remove('carrousel--ouvrir')
+    })
+    
+    
+    })()
+
+/* (function(){
+    // fonction IFEE
     console.log('debut du carrousel')
-})()
+    let boutonouvrir = document.querySelector('.bouton_ouvrir')
+    let elmCarrousel = document.quuerySelector('.carrousel')
+    let elmBouton__x = document.querySelector('.bouton__x')
+    console.log(boutonouvrir.tagName)
+    bouton__ouvrir.addeventlistener('mousedown', function(){
+        console.log('boite modale')
+        elmCarrousel.classlist.add('carrousel--ouvrir')
+    })
+    elmBouton__x.addeventlistener('mousedown', function(){
+        console.log('boite modale')
+        elmCarrousel.classlist.add('carrousel--ouvrir')
+    })
+})() */
