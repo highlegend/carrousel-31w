677ff3639ae7737f55f37cb9be4775a97e3a32da
Mehdi Said <e1489401@cmaisonneuve.qc.ca>
2023-03-15 10:12:29 -0400

    c13 ajustements des elements de controle du carrousel

--- carroussel.php
+++ carroussel.php
@@ -22,21 +22,21 @@ function carrousel_enqueue(){
 
     $version_css = filemtime(plugin_dir_path(__FILE__ ) . "style.css");
     $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
-
+    
     wp_enqueue_style(   'em_plugin_carrousel_css',
     plugin_dir_url(__FILE__) . "style.css",
     array(),
     $version_css);
-
+    
     wp_enqueue_script(  'em_plugin_carrousel_js',
     plugin_dir_url(__FILE__) ."js/carrousel.js",
     array(),
     $version_js,
     true);
     }
-
+    
     add_action('wp_enqueue_scripts', 'carrousel_enqueue');
-
+    
     function creation_carrousel()
     {
     return '<button class="bouton__ouvrir">Ouvrir</button>
@@ -46,5 +46,5 @@ function carrousel_enqueue(){
         <form class="carrousel__form"></form>
         </div>';
     }  
-
+    
     add_shortcode('carrousel', 'creation_carrousel');
